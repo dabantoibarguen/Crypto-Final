81ecbfef2c7c41807995b3a3a76897827943e3f7
Matthew Raphael Rojas <94476790+mrojs@users.noreply.github.com>
2022-12-12 17:20:58 -0500

    Update mycontract.sol
    done :)

--- contracts/mycontract.sol
+++ contracts/mycontract.sol
@@ -8,6 +8,31 @@ import "hardhat/console.sol";
 contract Splitwise {
 // DO NOT MODIFY ABOVE THIS
 
+
 // ADD YOUR CONTRACT CODE BELOW
+
+    mapping(address => mapping (address => uint32)) public debtMap;
+
+    function lookup(address debtor, address creditor) public view returns (uint32 ret){
+        return debtMap[debtor][creditor];
+    }
+
+    function add_IOU(address creditor, uint32 amount)  public {
+        if (creditor != msg.sender){
+            if(debtMap[creditor][msg.sender] > 0){
+                uint32 temp = debtMap[creditor][msg.sender];
+                if(amount<temp){
+                    debtMap[creditor][msg.sender] -= amount;
+                }
+                else{
+                    debtMap[msg.sender][creditor] += amount-temp;
+                    debtMap[creditor][msg.sender] -= temp;
+                }
+            }
+            else{
+                debtMap[msg.sender][creditor] += amount;
+            }
+            }  
+    }
 
 }
